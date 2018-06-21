5f956f0e95bed2c86baa7eea5a78799505bed2d7
sahulhamee <40460781+sahulhamee@users.noreply.github.com>
2018-06-21 16:46:29 +0530

    testste
    etstse

---
+++ TESTWEBSITE/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TESTWEBSITE.Startup))]
+namespace TESTWEBSITE
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
